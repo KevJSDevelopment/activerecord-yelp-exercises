3eae99b44adbf8c9a09a3937c32d958a0cf280a7
KevJSDevelopment <Kshhn12@gmail.com>
2020-09-14 18:00:47 -0400

    restaurant methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -19,6 +19,41 @@ class Restaurant < ActiveRecord::Base
     end
 
     def self.max_dishes
+        dish_counts = self.dish_count
+        sorted_dish_counts = dish_counts.sort_by{|key, value| value}
+        return Restaurant.find do |restaurant|
+            restaurant[:id] == sorted_dish_counts[sorted_dish_counts.count - 1][0]
+        end
+    end
+
+    def self.focused
+        dish_counts = self.dish_count
+        dish_counts.select do |key, value|
+            value < 5
+        end
+    end
+
+    def self.dish_count
+        Dish.group(:restaurant_id).count
+    end
+
+    def self.large_menu
+        dish_counts = self.dish_count
+        dish_counts.select do |key, value|
+            value < 20
+        end
+    end
+
+    def self.vegetarian
+        Tag.where("name = 'Vegetarian'")
+    end
+
+    def self.name_like(n)
+        Restaurant.where("name = '#{n}'")
+    end
+
+    def self.name_not_like(n)
+        Restaurant.where("name != '#{n}'")
     end
 
     # def dishes
